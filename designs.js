581b5da7163f8e3e23ec9a25747f788c10cd7046
JayLan <jaylan@bu.edu>
2018-06-18 22:50:18 -0400

    Added create grid functionality.

--- designs.js
+++ designs.js
@@ -1,10 +1,37 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
+
+
+var sub_grid = document.getElementById('sub_1');
+
+const CANVAS = document.getElementById("pixelCanvas");
 
 function makeGrid() {
-  // Your code goes here!
+  
+  if(CANVAS.firstChild){
+    CANVAS.removeChild(CANVAS.firstChild);
+  }
+
+  const T_BOD = document.createElement("tbody");
+  const ROWS = document.getElementById("inputHeight").value;
+  const COLS = document.getElementById("inputWidth").value;
+
+  for (let i = 1; i <= ROWS; i++) {
+    const T_ROW = document.createElement("tr");
+
+    for (let j = 1; j <= COLS; j++) {
+      const T_COL = document.createElement("td");
+      T_ROW.appendChild(T_COL);
+    }
+    T_BOD.appendChild(T_ROW );
+  }
+  CANVAS.appendChild(T_BOD);
+
 }
 
+sub_grid.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  console.log('sub clicked!');
 
+  makeGrid();
+
+});
